0aaa50d0b57fe0aafaddc93b26ba1c1cdb8e962a
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-04-01 20:48:38 +0530

    Added Services

--- src/parts/Portfolio.jsx
+++ src/parts/Portfolio.jsx
@@ -2,65 +2,94 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prop-types */
 import React from "react";
-import { Fade } from "react-awesome-reveal";
+
 import Button from "@elements/button";
+import { motion } from "framer-motion";
 export default function Portfolio({ data }) {
   return (
-    <section className="container flex flex-col items-center mx-auto mt-20">
-      <div>
+    <section className="container flex flex-col items-center mx-auto mt-20 w-[100%]">
+      <motion.div
+        initial={{
+          opacity: 0,
+
+          y: 100,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: {
+            duration: 0.5,
+          },
+        }}
+        viewport={{ once: true }}
+      >
         <h1 className="text-5xl font-bold text-center text-theme-blue">
           Our Selected Project
         </h1>
-        <p className="mx-3 mb-12 text-lg font-light text-center text-gray-400 md:mx-auto">
+        <p className="mb-12 text-lg font-light text-center text-gray-400">
           We are ready to scale up your business with our great work result.
         </p>
-      </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 sm:gap-2 xl:gap-8 justify-items-center">
+      </motion.div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 sm:gap-2 xl:gap-8 justify-items-center w-[100%]">
         {data.map((item, index) => (
           <Button type="link" href={`/project/${item.id}`} key={index}>
-            <Fade cascade damping={0.5} direction="up" triggerOnce>
-              <div className="w-11/12 m-3 transition duration-500 transform shadow-xl group rounded-2xl hover:scale-110 portofolio-card">
-                <div className="relative">
-                  <img
-                    src={item.imageUrl}
-                    alt="Portfolio"
-                    className="z-0 rounded-t-2xl"
-                  />
-                  <div className="absolute top-0 flex justify-center w-full h-full bg-black rounded-b opacity-0 rounded-t-2xl img-hover">
-                    <button className="focus:outline-none">
-                      <svg
-                        className="w-20 h-20 text-gray-200"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <div className="py-4">
-                  <h2 className="text-xl text-center text-theme-blue">
-                    {item.title}
-                  </h2>
-                  <p className="font-light text-center text-gray-400">
-                    {item.type}
-                  </p>
+            <motion.div
+              initial={{
+                opacity: 0,
+
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  duration: 0.5,
+                  delay: index * 0.1,
+                },
+              }}
+              viewport={{ once: true }}
+              className="w-11/12 m-3 transition duration-500 transform shadow-xl group rounded-2xl hover:scale-110 portofolio-card"
+            >
+              <div className="relative">
+                <img
+                  src={item.imageUrl}
+                  alt="Portfolio"
+                  className="z-0 rounded-t-2xl"
+                />
+                <div className="absolute top-0 flex justify-center w-full h-full bg-black rounded-b opacity-0 rounded-t-2xl img-hover">
+                  <button className="focus:outline-none">
+                    <svg
+                      className="w-20 h-20 text-gray-200"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>
+                  </button>
                 </div>
               </div>
-            </Fade>
+              <div className="py-4">
+                <h2 className="text-xl text-center text-theme-blue">
+                  {item.title}
+                </h2>
+                <p className="font-light text-center text-gray-400">
+                  {item.type}
+                </p>
+              </div>
+            </motion.div>
           </Button>
         ))}
       </div>
--- src/parts/Service.jsx
+++ src/parts/Service.jsx
@@ -1,62 +1,38 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prop-types */
 import React from "react";
-import { motion } from "framer-motion";
+import { Fade } from "react-awesome-reveal";
 export default function Service({ data }) {
   return (
     <div className="bg-gray-50">
-      <div className="container pt-20 pb-28">
-        <motion.div
-          initial={{
-            opacity: 0,
+      <div className="container pt-20 mx-auto pb-28">
+        <h1 className="text-5xl font-bold text-center text-theme-blue">
+          Our Service
+        </h1>
 
-            y: 100,
-          }}
-          whileInView={{
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration: 0.5,
-            },
-          }}
-          viewport={{ once: true }}
-        >
-          {" "}
-          <h1 className="text-5xl font-bold text-center text-theme-blue">
-            Our Service
-          </h1>
-          <p className="mx-3 mb-12 text-lg font-light text-center text-gray-400 md:mx-auto">
-            We are ready to scale up your business with our great service.
-          </p>
-        </motion.div>
+        <p className="mb-12 text-lg font-light text-center text-gray-400">
+          We are ready to scale up your business with our great service.
+        </p>
+
         <div className="grid grid-rows-3 gap-8 px-10 sm:grid-cols-3 sm:grid-rows-1 sm:gap-6 xl:gap-16">
           {data.map((item, index) => (
-            <motion.div
-              initial={{
-                opacity: 0,
-
-                y: 100,
-              }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-                transition: {
-                  duration: 0.5,
-                },
-              }}
-              viewport={{ once: true }}
-            >
-              <div className="transition duration-500 transform bg-white border shadow-2xl group rounded-2xl border-light-theme-purple hover:scale-105">
-                <img
-                  src={item.imageUrl}
-                  alt="Service"
-                  className="w-full rounded-t-2xl"
-                />
-                <h2 className="text-xl text-center text-theme-blue py-7 rounded-b-2xl">
-                  {item.title}
-                </h2>
-              </div>
-            </motion.div>
+            <div>
+              <Fade direction="up" triggerOnce>
+                <div
+                  className="transition duration-500 transform bg-white border shadow-2xl group rounded-2xl border-light-theme-purple hover:scale-105"
+                  key={index}
+                >
+                  <img
+                    src={item.imageUrl}
+                    alt="Service"
+                    className="w-full rounded-t-2xl"
+                  />
+                  <h2 className="text-xl text-center md:text-sm lg:text-xl text-theme-blue py-7 rounded-b-2xl">
+                    {item.title}
+                  </h2>
+                </div>
+              </Fade>
+            </div>
           ))}
         </div>
       </div>
